0021cec07171c820431acbbaf359e8b24cd81d6e
Wanuja Ranasinghe <63337783+Wanuja97@users.noreply.github.com>
2022-09-10 02:23:03 +0530

    animations added

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,14 +18,14 @@ export default function Home() {
 
       <main className={Styles.main}>
         <div className={Styles.uppercontent}>
-        <div className={Styles.content}>
+        <div className={`${Styles.content}  animate__animated animate__zoomIn `}>
           <p className={`${Styles.welcome} gradientcolor`}>Welcome to Programmingster.</p>
           <h1>Sharing the Knowledge is the best thing to learn.</h1>
           <div className={Styles.btncontainer}>
             <a href="#categories"><Button className={Styles.startbtn}>Get Started</Button></a>
           </div>
         </div>
-        <div className={Styles.homeimage}>
+        <div className={`${Styles.homeimage}  animate__animated animate__zoomIn `}>
           <Image
           src={HomeImage}
           alt="Home Image"
