39962f463791e59237857fb980c7a7fe2c5acc3d
Wanuja Ranasinghe <63337783+Wanuja97@users.noreply.github.com>
2022-09-13 02:38:24 +0530

    frontend structure completed

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Styles from '../styles/AboutPage.module.scss'
 export default function About() {
   return (
     <div>
@@ -9,7 +10,21 @@ export default function About() {
         
       </Head>
       <main className='commonpagestyles'>
-        About
+      <div className={Styles.aboutcontainer}>
+          <div className={Styles.heading}>
+            <h1 className="gradientcolor">About Programmingster</h1>
+          </div>
+          <div className={Styles.content}>
+            <p>
+            Welcome To Programmingster!!! <br /><br /> Programmingster is a Professional Blog Platform.
+            The purpose of creating this website was my passion for programming and, sharing my knowledge about information technology with people who are interested in and searching for it all over the world. I hope you enjoy Programmingster as much as I enjoy offering them to you. <br />
+            I will keep posting more important things on the Programmingster for all of you. Please give your support and love. <br />
+            <br /> Thanks For Visiting Programmingster. <br /><br />
+            <a href="https://www.wanujaranasinghe.me/">Wanuja Ranasinghe</a> <br />
+            - Author of Programmingster
+            </p>
+          </div>
+        </div>
       </main>
     </div>
   )
