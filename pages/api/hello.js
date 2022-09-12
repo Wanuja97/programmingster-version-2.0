b110a2a78dd8d3972b3712406bcd7528ebbe1162
Wanuja Ranasinghe <63337783+Wanuja97@users.noreply.github.com>
2022-09-13 02:06:14 +0530

    Home page structure done

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,3 +3,12 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+// export default async (req, res) => {
+//   const { method } = req;
+
+//   // This will allow OPTIONS request
+//   if (method === "OPTIONS") {
+//     return res.status(200).send("ok");
+//   }
+// };
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import HomeImage from '../public/homeimage-min.png'
+import HomeImage from "../public/homeimage-min.png";
 import Styles from "../styles/HomePage.module.scss";
 import Button from "react-bootstrap/Button";
+import HomeCategoryTabs from "../components/HomeCategoryTabs";
 export default function Home() {
   return (
     <div>
@@ -12,31 +13,40 @@ export default function Home() {
           name="description"
           content="Welcome to programmingster.Sharing knowledge is the best thing to learn"
         />
-        
       </Head>
 
       <main className={`${Styles.main} commonpagestyles`}>
         <div className={Styles.uppercontent}>
-        <div className={`${Styles.content}  animate__animated animate__zoomIn `}>
-          <p className={`${Styles.welcome} gradientcolor`}>Welcome to Programmingster.</p>
-          <h1>Sharing knowledge is the best thing to learn.</h1>
-          <div className={Styles.btncontainer}>
-            <a href="#categories"><Button className={Styles.startbtn}>Get Started</Button></a>
+          <div
+            className={`${Styles.content}  animate__animated animate__zoomIn `}
+          >
+            <p className={`${Styles.welcome} gradientcolor`}>
+              Welcome to Programmingster.
+            </p>
+            <h1>Sharing knowledge is the best thing to learn.</h1>
+            <div className={Styles.btncontainer}>
+              <a href="#categories">
+                <Button className={Styles.startbtn}>Get Started</Button>
+              </a>
+            </div>
+          </div>
+          <div
+            className={`${Styles.homeimage}  animate__animated animate__zoomIn `}
+          >
+            <Image
+              src={HomeImage}
+              alt="Home Image"
+              priority={true}
+              width={933}
+              height={700}
+            />
           </div>
         </div>
-        <div className={`${Styles.homeimage}  animate__animated animate__zoomIn `}>
-          <Image
-          src={HomeImage}
-          alt="Home Image"
-          priority={true}
-          width={933}
-          height={700}
-          />
-        </div>
-        </div>
 
-        <div className={Styles.posts} id="categories">
-          <h2>Hello</h2>
+        <div className={Styles.bottomcontent}>
+          <div className={Styles.posts} id="categories">
+            <HomeCategoryTabs />
+          </div>
         </div>
       </main>
     </div>
